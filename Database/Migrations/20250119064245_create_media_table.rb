a326003acde771aa8b9758711b1fd0fdd7213c57
yadon <doinkya0@gmail.com>
2025-02-08 09:29:25 +0000

    Fix: media table migration script

--- Database/Migrations/20250119064245_create_media_table.rb
+++ Database/Migrations/20250119064245_create_media_table.rb
@@ -2,7 +2,7 @@ class CreateMediaTable
   def up(db)
       db.query(<<-SQL)
       CREATE TABLE IF NOT EXISTS media (
-        id BIGINT AUTO_INCREMENT PRIMARY KEY
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
         post_id BIGINT,
         media_type VARCHAR(50),
         media_url VARCHAR(255),
