9c2fa0f1201f5588363bf615638b65a1bc4d8792
yadon <doinkya0@gmail.com>
2025-03-29 07:46:15 +0000

    fix: api/postsのルートを修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,12 +8,21 @@ MyRouter = Proc.new do |env|
 
   puts "DEBUG: method=#{method.inspect}, path_info=#{path.inspect}"
 
-  case [method, path]
-  when ['GET', '/']
-    HomeController.new.index(req)
-  when ['GET', %r{^/api/posts$}]
-    PostsController.new.timeline(req)
-  else
-    [404, { "Content-Type" => "application/json" }, [{ error: "Not Found" }.to_json]]
+  case method
+  when 'GET'
+    if path =~ %r{^/api/posts/(\d+)$}
+      # $1 には (\d+) にマッチした文字列が入る(=postのid)
+      post_id = $1.to_i
+      return PostsController.new.show(req, post_id)
+    end
+  
+    # 既存の /api/posts 一覧をマッチ
+    if path == '/api/posts'
+      return PostsController.new.timeline(req)
+    end
+  
+    # 他のルート...
   end
+  
+  [404, { 'Content-Type' => 'application/json' }, [{ error: 'Not Found' }.to_json]]  
 end
